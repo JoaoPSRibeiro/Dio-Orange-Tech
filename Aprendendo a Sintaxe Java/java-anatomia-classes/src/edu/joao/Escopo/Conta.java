2e9bf99fa1fafa4b1d69be5395ab6c77b6db5cfe
João Paulo Santos Ribeiro <99143955+JoaoPSRibeiro@users.noreply.github.com>
2023-01-24 09:03:00 -0300

    Logica condicional - inicio

---
+++ Aprendendo a Sintaxe Java/java-anatomia-classes/src/edu/joao/Escopo/Conta.java
@@ -0,0 +1,34 @@
+package edu.joao.Escopo;
+/*
+
+public class Conta{
+      
+    // variavel da classe conta
+    double saldo = 10.0;
+
+    public void sacar(Double valor){ //método
+        // variavel local de método
+        double novoSaldo = saldo - valor;
+    }
+    public void imprimirSaldo(){
+        //disponivel em toda classe
+        System.out.println(saldo);
+        //Somente o método sacar comnhece esta variável
+        System.out.println(novoSaldo);
+    }
+    public double calcularDividaExponencial(){ //método
+        //variável local de método
+        double valorParcela = 50.0;
+        double valorMontante = 0.0; // começando a variável
+        for (int x=1, x<=5, x++){
+        // x variável de escopo
+        //esta variável será reiniciada a cada execução
+        double valorCalculado = valorParcela * x;
+        valorMontante = valorMontante + valorCalculado;
+        }
+        //escopo de fluxo
+        //x e valorCalculado nunca estarão disponíveis fora do fluxo
+        return valorMontante;
+    }
+}
+ */
